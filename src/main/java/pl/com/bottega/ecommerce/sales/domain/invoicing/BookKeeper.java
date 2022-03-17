44952bd206f33fb827cb949e97ee54b6b2ab3bd4
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-03-17 17:35:49 +0100

    usage of InvoiceRequest in BookKeeper class

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -13,21 +13,19 @@
 package pl.com.bottega.ecommerce.sales.domain.invoicing;
 
 import java.math.BigDecimal;
-import java.util.List;
 
-import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.ClientData;
 import pl.com.bottega.ecommerce.canonicalmodel.publishedlanguage.Id;
 import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeper {
 
-    public Invoice issuance(ClientData client, List<RequestItem> items) {
-        Invoice invoice = Invoice.createInvoiceObject(Id.generate(), client);
+    public Invoice issuance(InvoiceRequest invoiceRequestObject) {
+        Invoice invoice = Invoice.createInvoiceObject(Id.generate(), invoiceRequestObject.getClient());
 
-        for (RequestItem item : items) {
+        for (RequestItem item : invoiceRequestObject.getItems()) {
             Money net = item.getTotalCost();
-            BigDecimal ratio = null;
-            String desc = null;
+            BigDecimal ratio;
+            String desc;
 
             switch (item.getProductData().getType()) {
                 case DRUG:
