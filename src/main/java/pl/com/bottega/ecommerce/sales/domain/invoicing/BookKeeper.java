79790f8333655304385aca38e9f08e6ed2d87799
Lukasz Krulik <228780@edu.p.lodz.pl>
2022-03-17 18:21:58 +0100

    Extracted TaxCalculating part of the code to class

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/BookKeeper.java
@@ -19,38 +19,11 @@ import pl.com.bottega.ecommerce.sharedkernel.Money;
 
 public class BookKeeper {
 
-    public Invoice issuance(InvoiceRequest invoiceRequestObject) {
+    public Invoice issuance(InvoiceRequest invoiceRequestObject, DefaultTaxCalculator defaultTaxCalculator) {
         Invoice invoice = Invoice.createInvoiceObject(Id.generate(), invoiceRequestObject.getClient());
 
         for (RequestItem item : invoiceRequestObject.getItems()) {
-            Money net = item.getTotalCost();
-            BigDecimal ratio;
-            String desc;
-
-            switch (item.getProductData().getType()) {
-                case DRUG:
-                    ratio = BigDecimal.valueOf(0.05);
-                    desc = "5% (D)";
-                    break;
-                case FOOD:
-                    ratio = BigDecimal.valueOf(0.07);
-                    desc = "7% (F)";
-                    break;
-                case STANDARD:
-                    ratio = BigDecimal.valueOf(0.23);
-                    desc = "23%";
-                    break;
-
-                default:
-                    throw new IllegalArgumentException(item.getProductData().getType() + " not handled");
-            }
-
-            Money taxValue = net.multiplyBy(ratio);
-
-            Tax tax = new Tax(taxValue, desc);
-
-            InvoiceLine invoiceLine = new InvoiceLine(item.getProductData(), item.getQuantity(), net, tax);
-            invoice.addItem(invoiceLine);
+            invoice.addItem(defaultTaxCalculator.generateInvoiceLine(item));
         }
 
         return invoice;
